262f5a63ab363a30f9064afb4dd27d3b01a7d57c
thigasmonteiro <thiagodepaulamonteiro@gmail.com>
2023-10-26 15:15:55 -0300

    Criação da Api e Url de Categorias

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
-from .models import Categoria, Editora, Autor
+from .models import Categoria, Editora, Autor, Livro
 
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
 admin.site.register(Autor)
+admin.site.register(Livro)
+
 # Register your models here.
--- livraria/views.py
+++ livraria/views.py
@@ -1,3 +1,16 @@
+#queryset = Categoria.objects.all(): define o conjunto de objetos que será retornado pela view.
+#serializer_class = CategoriaSerializer: define o serializer que será utilizado para serializar os objetos.
+
+
 from django.shortcuts import render
+
+from rest_framework.viewsets import ModelViewSet
+
+from livraria.models import Categoria
+from livraria.serializers import CategoriaSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
 
 # Create your views here.
