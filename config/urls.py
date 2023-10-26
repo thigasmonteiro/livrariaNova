57ee961a2c7cf18708c90a6662d2de0204ba7a34
thigasmonteiro <thiagodepaulamonteiro@gmail.com>
2023-10-26 15:40:23 -0300

    Criacao Api Autor

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,15 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
-from livraria.views import CategoriaViewSet
+from livraria.views import CategoriaViewSet, EditoraViewSet, AutorViewSet
 
 router = DefaultRouter()
-router.register(r"categorias", CategoriaViewSet)
+router.register(r"categorias" , CategoriaViewSet)
+router.register(r"editoras" , EditoraViewSet)
+router.register(r"autor" , AutorViewSet)
+
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
